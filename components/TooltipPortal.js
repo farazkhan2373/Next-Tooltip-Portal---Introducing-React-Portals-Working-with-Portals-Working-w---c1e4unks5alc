5b21692a480bc39b46dc25a739c77a1b1ddf3500
farazkhan2373 <136571344+farazkhan2373@users.noreply.github.com>
2023-10-02 16:09:26 +0530

    Update TooltipPortal.js

--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -1,9 +1,24 @@
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
+ const portalElement = document.createElement('div');
 
+  useEffect(() => {
+    if (portalRoot) {
+      portalRoot.appendChild(portalElement);
+    }
+
+    return () => {
+      if (portalRoot) {
+        portalRoot.removeChild(portalElement);
+      }
+    };
+  }, []);
+
+  return ReactDOM.createPortal(children, portalElement);
 }
 
 export default TooltipPortal;
