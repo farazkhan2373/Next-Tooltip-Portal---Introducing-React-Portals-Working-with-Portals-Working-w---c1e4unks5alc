867392cbfa74fe45ea5f2197fb0f66b593298e8c
farazkhan2373 <136571344+farazkhan2373@users.noreply.github.com>
2023-10-02 16:11:33 +0530

    Update DynamicTooltip.js

--- components/DynamicTooltip.js
+++ components/DynamicTooltip.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
 import TooltipPortal from './TooltipPortal';
 
 function DynamicTooltip() {
@@ -9,7 +10,7 @@ function DynamicTooltip() {
   return (
     <TooltipPortal>
       <div style={{ position: 'absolute', top: '50px', left: '50px', backgroundColor: 'gray', padding: '10px', borderRadius: '5px' }}>
-   
+       This is a tooltip
       </div>
     </TooltipPortal>
   );
